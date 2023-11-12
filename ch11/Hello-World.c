4b9e42fea01a452fc02866d32ac5dbefdabd40fb
Srikar KC <sclifeisgreat@gmail.com>
2023-11-12 09:54:55 -0800

    Added high level languages basic code

---
+++ ch11/Hello-World.c
@@ -0,0 +1,10 @@
+/* This program prints out Hello World to the STDOUT */
+
+/* Pre processor directive */
+#include<stdio.h>
+
+int main(int argc, char **argv)
+{
+	puts("Hello World!");
+	return 0;
+}
